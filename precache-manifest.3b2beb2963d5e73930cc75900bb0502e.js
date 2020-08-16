3fd6228ee2b505e8e412b49fdd46bcb50bc9afc7
Debaprasad-dez <debaprasadpaul033@gmail.com>
2020-08-17 01:03:10 +0530

    Updates

---
+++ precache-manifest.3b2beb2963d5e73930cc75900bb0502e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200bfb72f3d1bb2643f706cbeb54afa0",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "32621218ab7f99301ec2",
+    "url": "/covid19/static/css/main.5bef7ebe.chunk.css"
+  },
+  {
+    "revision": "30fd4807f02ef0a8610e",
+    "url": "/covid19/static/js/2.02b7d34e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19/static/js/2.02b7d34e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32621218ab7f99301ec2",
+    "url": "/covid19/static/js/main.7c690b62.chunk.js"
+  },
+  {
+    "revision": "2462338afa2528633551",
+    "url": "/covid19/static/js/runtime-main.3bc255e0.js"
+  }
+]);
